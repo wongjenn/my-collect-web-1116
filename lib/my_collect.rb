f78deaaedb372fe31f1e66bbbcce882416383bb3
Jennifer Wong <wongjenn@yahoo.com>
2016-10-28 22:58:18 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect(array)
+  new_array = []
+  i = 0
+  while i < array.length
+    result = yield(array[i])
+    puts result
+    new_array.push(result)
+    i += 1
+  end
+  new_array
+end
+
+my_collect(['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']) do |student|
+  student.split(" ").first
+end
 
 
+my_collect(['ruby', 'javascript', 'python', 'objective-c']) do |langauge|
+  langauge.upcase
+end
